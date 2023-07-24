e4b35304f06497b167e6c0f0c64ce0d948853127
Bolong Wang <wangb@gmx.com>
2023-07-23 21:59:18 -0700

    Generate demo project with Spring Initializr
    Reference: https://start.spring.io/

---
+++ src/main/java/dev/wbolong/demo/rabbitmanagerspringboot/RabbitManagerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package dev.wbolong.demo.rabbitmanagerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitManagerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitManagerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wbolong/demo/rabbitmanagerspringboot/RabbitManagerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wbolong.demo.rabbitmanagerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitManagerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
