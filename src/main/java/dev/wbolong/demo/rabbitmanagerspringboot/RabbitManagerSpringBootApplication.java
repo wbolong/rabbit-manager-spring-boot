1d8991e582dc20ab68f4bd573c2dd667d679812e
Bolong Wang <wangb@gmx.com>
2023-07-23 22:22:44 -0700

    Autoformat code files using spaces

--- src/main/java/dev/wbolong/demo/rabbitmanagerspringboot/RabbitManagerSpringBootApplication.java
+++ src/main/java/dev/wbolong/demo/rabbitmanagerspringboot/RabbitManagerSpringBootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RabbitManagerSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RabbitManagerSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitManagerSpringBootApplication.class, args);
+    }
 
 }
--- src/test/java/dev/wbolong/demo/rabbitmanagerspringboot/RabbitManagerSpringBootApplicationTests.java
+++ src/test/java/dev/wbolong/demo/rabbitmanagerspringboot/RabbitManagerSpringBootApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class RabbitManagerSpringBootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
